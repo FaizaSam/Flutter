8785956d9ce8e23be3fd5f24311f943005044295
faizza.mohammed <faizza.mohammed@gmail.com>
2021-12-23 14:14:33 +0530

    Stateful Eidget App

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'Stateful app example',
+    home: FavouriteCity(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class FavouriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavouriteCityState();
+  }
+}
+
+class _FavouriteCityState extends State<FavouriteCity> {
+  String nameCity = "";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MyFirst App'),
+    debugPrint("Favourite city Widget is created");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful App Example'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  debugPrint(
+                      "Set State is called,This tells the widget to redraw the favourite city");
+                  nameCity = userInput;
+                });
+              },
+            ),
+            Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Text(
+                  "Your best City is $nameCity",
+                  style: TextStyle(fontSize: 20.0),
+                ))
+          ],
         ),
-        body: Text('This is my Default text'),
       ),
     );
   }
